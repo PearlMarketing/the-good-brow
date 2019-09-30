6dda0bf1d8611ac0e3aa88986d901e05fb1ff93f
Culver Lau <culverlau@gmail.com>
2019-09-30 11:33:09 -0400

    removed border on collapsed, fixed titles to flex display

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -2,9 +2,12 @@ import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
 
 const StyledCollapse = styled.div`
+  margin:1rem 0;
   .collapse_button {
     width: 100%;
     height: auto;
+    display: flex;
+    align-items: center;
     padding: 0.75rem 1rem 0.75rem 1rem;
     border-radius: 0;
     p {
@@ -20,16 +23,10 @@ const StyledCollapse = styled.div`
         padding-top: 5px;
         display: block;
       }
-      .arrow {
-        position: absolute;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        font-size: 1.8rem;
-        transform-origin: 50% 45%;
-        transition: transform 0.3s;
-        --webkit-transition: transform 0.3s;
-      }
+    }
+    .arrow {
+      font-size:1.8rem;
+      line-height:1rem;
     }
   }
   .collapse_content {
@@ -37,6 +34,7 @@ const StyledCollapse = styled.div`
     border-left: 1px solid #fff;
     border-right: 1px solid #fff;
     max-height: 0;
+    visibility: hidden;
     overflow: hidden;
     transition: max-height 0.3s ease-out;
     --webkit-transition: max-height 0.3s ease-out;
@@ -49,13 +47,12 @@ const StyledCollapse = styled.div`
     .collapse_button {
       p {
         .arrow {
-          transform: rotate(-180deg);
-          transform-origin: 50% 45%;
         }
       }
     }
     .collapse_content {
       max-height: 500px;
+      visibility: visible;
     }
   }
 `
@@ -63,11 +60,13 @@ const StyledCollapse = styled.div`
 const Collapse = props => {
   const [setActive, setActiveState] = useState('')
   const [setHeight, setHeightState] = useState('0px')
+  const [setArrow, setArrowState] = useState('fa-angle-down')
 
   const content = useRef(null)
 
   function toggleCollapse() {
     setActiveState(setActive === '' ? 'active' : '')
+    setArrowState(setActive === 'active' ? 'fa-angle-down' : 'fa-angle-up')
     if (props.children) {
       setHeightState(
         setActive === 'active' ? '0px' : `${content.current.scrollHeight}px`
@@ -81,12 +80,12 @@ const Collapse = props => {
         <p>
           {props.title}
           {props.price ? <span className="price">{props.price}</span> : ''}
-          {props.children ? (
-            <span className="icon fa-angle-down arrow"></span>
-          ) : (
-            ''
-          )}
         </p>
+        {props.children ? (
+          <span className={`icon ${setArrow} arrow`}></span>
+        ) : (
+          ''
+        )}
       </button>
       <div
         ref={content}
