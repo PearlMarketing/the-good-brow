6e8006842cf20d24b6aaddf78862e6b1e5b0e8b8
Culver Lau <culverlau@gmail.com>
2019-11-14 10:14:28 -0500

    fixed body markdown insertion on various pages

--- src/components/sections/Microblading/index.js
+++ src/components/sections/Microblading/index.js
@@ -39,13 +39,7 @@ class Microblading extends React.Component {
             {/* <span className="image main">
             <img src={pic03} alt="" />
           </span> */}
-            <p>
-              Microblading is the manual process of implanting pigment beneath
-              the surface of the skin by means of tiny, sterile, disposable
-              needles to give the appearance of natural hairstrokes to enhance
-              your brows. If you are interested in effortless brows and boosting
-              your confidence, this is exactly what youve been searching for!
-            </p>
+            <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
             {close}
           </article>
         )}
--- src/components/sections/Policies/index.js
+++ src/components/sections/Policies/index.js
@@ -28,96 +28,23 @@ class Policies extends React.Component {
           }
         `}
         render={data => (
-      <article
-        id="policies"
-        className={`${this.props.article === 'policies' ? 'active' : ''} ${
-          this.props.articleTimeout ? 'timeout' : ''
-        }`}
-        style={{ display: 'none' }}
-      >
-        <h2 className="major">{data.markdownRemark.frontmatter.title}</h2>
-        {/* <span className="image main">
+          <article
+            id="policies"
+            className={`${this.props.article === 'policies' ? 'active' : ''} ${
+              this.props.articleTimeout ? 'timeout' : ''
+            }`}
+            style={{ display: 'none' }}
+          >
+            <h2 className="major">{data.markdownRemark.frontmatter.title}</h2>
+            {/* <span className="image main">
             <img src={pic03} alt="" />
           </span> */}
-        <p>
-          POLICIES FOR MICROBLADING
-          <br />
-          <br />
-          Please arrive 15 minutes before your scheduled appointment to fill out
-          paperwork.
-          <br />
-          <br />
-          There are many factors that affect the healing process that we have no
-          control over  (how you take care of your brow tattoo, how your skin
-          heals and retains pigment, skin type etc.). There is absolutely no
-          guarantee with healed results.
-          <br />
-          <br />
-          All appointments require a $100 deposit prior to booking.  Deposits
-          are non-refundable but transferable to re-schedule an appointment if
-          48 hours notice is given. The amount is deducted from the final cost,
-          which is due the day of your service.
-          <br />
-          <br />I accept Cash and major credit cards. I do not offer any
-          refunds.
-          <br />
-          <br />
-          As these appointments take considerable time, I require 48 hours of
-          notice to reschedule an appointment. Cancellations with less than 48
-          hours notice will be charged a $50 fee. If you cancel with less than
-          24 hours notice the full deposit will be automatically forfeited.
-          <br />
-          <br />
-          Please contact me if you are not able to make your appointment or if
-          you are running late. I understand last minuite circumstances happen
-          and I will do my best to accommodate you. Being more than 15 minutes
-          late may result in a forfeit of your deposit and re-scheduling of your
-          appointment.
-          <br />
-          <br />
-          NO SHOW APPOINTMENTS: The deposit will be forfeited if the client
-          fails to show up to their appointment. I respect your time, please
-          respect mine.
-          <br />
-          <br />
-          Prices are subject to change at anytime. You must be 18 years or have
-          Parental written consent to get Microblading.
-          <br />
-          <br />
-          Microblading is considered an invasive procedure, as a result you will
-          not be able to donate blood for one year.
-          <br />
-          <br />I do not do touch-ups on other artists work.
-          <br />
-          <br />
-          <br />
-          <br />
-           POLICIES FOR WAXING
-          <br />
-          <br />
-          First time clients, please arrive 15 minutes before your scheduled
-          appointment to fill out paperwork.
-          <br />
-          <br />
-          In order to hold any appointment over 30 minutes you must provide a
-          valid credit or debit card to place on file. Dont worry, you will not
-          be charged upon booking. You can still pay with a different card or
-          cash upon checking out.
-          <br />
-          <br />
-          If you are unable to keep your appointment I ask that you give me
-          24-hours advance notice. If you cancel with less than 24-hour notice
-          you are subject to a 50% fee.
-          <br />
-          <br />
-          If you do not show up for your appointment (no show), you will be
-          charged 100% of the full service price of all appointments booked.
-          <br />
-          <br />I respect your time, please respect mine. Thank you!
-        </p>
-        {close}
-      </article>
-      )}
+            <div
+              dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+            />
+            {close}
+          </article>
+        )}
       />
     )
   }
