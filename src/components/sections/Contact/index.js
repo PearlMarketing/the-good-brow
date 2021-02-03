7aceae5089bd82805f4138012feb56bc58981c7b
Culver Lau <culverlau@gmail.com>
2021-02-03 17:44:28 -0500

    removed iframe google maps from contact

--- src/components/sections/Contact/index.js
+++ src/components/sections/Contact/index.js
@@ -36,14 +36,6 @@ class Contact extends React.Component {
             style={{ display: 'none' }}
           >
             <h2 className="major">{data.markdownRemark.frontmatter.title}</h2>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d181.77555035546533!2d-71.53423466946826!3d43.200914195932995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e26a8e64af7e0d%3A0x3fd0fe812309399!2s48%20S%20Main%20St%2C%20Concord%2C%20NH%2003301!5e0!3m2!1sen!2sus!4v1569818448684!5m2!1sen!2sus"
-              width="100%"
-              height="450"
-              frameborder="0"
-              style={{ border: 0 }}
-              allowfullscreen=""
-            ></iframe>
             <div
               dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
             />
